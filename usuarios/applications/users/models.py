01592257abe351b6ead6cc0257d8a3a508a1ce99
Francisco Pizarro <frpizarrob@gmail.com>
2022-03-04 13:36:56 -0300

    Configuracion-2

---
+++ usuarios/applications/users/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+class User(AbstractBaseUser, PermissionsMixin):
+    
+    GENDER_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+        ('O', 'Otro'),
+    )
+
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField()
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    sexo = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+
+    USERNAME_FIELD = 'username'
+
+    def get_short_name(self):
+        return self.username
+
+    def get_full_name(self):
+        return self.nombre + ' ' + self.apellido
