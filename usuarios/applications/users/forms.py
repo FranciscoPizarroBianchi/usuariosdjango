b5cc021856c151076b86d8dd1c7f733d00f2a896
Francisco Pizarro <frpizarrob@gmail.com>
2022-03-11 15:55:11 -0300

    Actualizacion

---
+++ usuarios/applications/users/forms.py
@@ -0,0 +1,94 @@
+from django import forms
+from django.contrib.auth import authenticate
+
+from .models import User
+
+class UserRegisterForm(forms.ModelForm):
+    
+    password1  = forms.CharField(
+        label = 'contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña'
+            }
+        )
+    )
+
+    password2  = forms.CharField(
+        label = 'contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Repetir contraseña'
+            }
+        )
+    )
+
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'nombre',
+            'apellido',
+            'sexo'
+        )
+
+    def clean_password2(self):
+        if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+            self.add_error('password2', 'Las constraseñas no coinciden')
+
+
+class LoginForm(forms.Form):
+    username  = forms.CharField(
+        label = 'username',
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'username',
+                'style':'{margin: 10px }',
+            }
+        )
+    )
+    password = forms.CharField(
+        label = 'contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña'
+            }
+        )
+    )
+
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+
+        if not authenticate(username=username,password=password):
+            raise forms.ValidationError('Los datos de usuario no son correctos')
+        
+        return self.cleaned_data
+
+
+class UpdatePasswordForm(forms.Form):
+    password1 = forms.CharField(
+        label = 'contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña actual'
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label = 'contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña nueva'
+            }
+        )
+    )
--- usuarios/applications/users/models.py
+++ usuarios/applications/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
     
@@ -12,11 +13,17 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField()
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=30, blank=True)
+    apellido = models.CharField(max_length=30, blank=True)
     sexo = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    codregistro = models.CharField(max_length=6, blank=True)
 
     USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email',]
+
+    objects = UserManager()
 
     def get_short_name(self):
         return self.username
